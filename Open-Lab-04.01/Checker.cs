9be96f7622df0022ac1ca05e28a9251246ac598b
samuelmalik <samuelmalik9@gmail.com>
2022-09-22 08:36:07 +0200

    added function returning if string has has two consecutive letters

--- Open-Lab-04.01/Checker.cs
+++ Open-Lab-04.01/Checker.cs
@@ -6,7 +6,16 @@ namespace Open_Lab_04._01
     {
         public bool DoubleLetters(string str)
         {
-            throw new NotImplementedException();
+            bool has2letters = false;
+            for (int i = 0; i < str.Length-1; i++)
+            {
+                has2letters = str[i] == str[i + 1];
+                if (has2letters == true)
+                {
+                    break;
+                }
+            }
+            return has2letters;
         }
     }
 }
